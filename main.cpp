c5962a726509ac13bdba0d1f7194695ab8bce499
LazyBoy92 <dimasgustiwijaya93@gmail.com>
2024-09-23 21:39:46 +0700

    seleksi olimpiade

--- main.cpp
+++ main.cpp
@@ -1,11 +1,52 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 int main()
 {
     int t,n,m;
+    
+    string player[81];
+    int arr[6][6];
+    int tmp[5];
 
-    int arr;
 
+
+    cin >> t;
+
+    string id;
+
+    for (int i = 1; i <= t; i++){
+        cin >> n >> m;
+        cin >> id;
+
+        for(int j = 1; j <= n;j++){
+            cin >> player[j] >> arr[j][1] >> arr[j][2] >> arr[j][3];
+            if(player[j] == id){
+                tmp[1] = arr[j][1];
+                tmp[2] = arr[j][2];
+                tmp[3] = arr[j][3];
+            }
+        }  
+
+    int x = 1;
+   for (int j = 1; j <= n; j++)
+   {
+     if(arr[j][3] > tmp[3]){
+        x++;
+     }else if(arr[j][3] == tmp[3] && arr[j][2] > tmp[2]){
+        x++;
+     }else if(arr[j][3] == tmp[3] && arr[j][2] == tmp[2] && arr[j][1] > tmp[1]){
+        x++;
+     }
+   }
+    
+    if(x <= m){
+        cout << "YA"<<endl;
+    }else{
+        cout << "TIDAK" <<endl;
+    }
+
+    }
+   
 
 }
